ff57c50676022b7bea94c4da0c24cf77ab4a6bd9
Sam Griffiths <sam.griffiths113@gmail.com>
2018-05-16 21:01:09 +0100

    Completed rocket tutorial with iPhone X constraints, images, labels, sound and animations

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -7,21 +7,45 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    var firstVariable = "Hello World."
+    
+    @IBOutlet weak var darkblueBG: UIImageView!
+    @IBOutlet weak var powerBtn: UIButton!
+    @IBOutlet weak var cloudHolder: UIView!
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var husterLbl: UILabel!
+    @IBOutlet weak var onLbl: UILabel!
+    
+    var player: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError{
+            print(error.description)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func powerBtnPressed(_ sender: Any) {
+        cloudHolder.isHidden = false
+        darkblueBG.isHidden = true
+        powerBtn.isHidden = true
+        
+        player.play()
+        
+        //Animate rocket.
+        UIView.animate(withDuration: 2.3, animations: {
+            self.rocket.frame = CGRect(x: 0, y: 140, width: 375, height: 402)
+        }) { (finished) in
+            self.husterLbl.isHidden = false
+            self.onLbl.isHidden = false
+        }
     }
-
-
 }
-
